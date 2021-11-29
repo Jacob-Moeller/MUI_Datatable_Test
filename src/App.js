92b55d2e29bcc83ef74d9863b8a6cff315a54f24
Jacob Moeller <86249005+Jacob-Moeller@users.noreply.github.com>
2021-11-29 16:42:20 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import ExpandableRowTable from "./ExpandableRowTable";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ExpandableRowTable />
+    </div>
+  );
+}
